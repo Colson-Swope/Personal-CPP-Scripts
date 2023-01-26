447fd42417ab02dc29b3b9949e2725ec719c8252
Colson-Swope <colsonjswope@gmail.com>
2023-01-25 18:07:28 -0800

    Initial Commit

---
+++ Tetris/Tetris/Tetris.cpp
@@ -0,0 +1,11 @@
+// Tetris.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+}
